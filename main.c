7eb3f014b2668e2164aa385918b73e0537eeffaf
tusharhero <tusharhero@sdf.org>
2023-10-03 15:14:30 +0530

    Implement a tokenizer to tokenize the input.

---
+++ main.c
@@ -0,0 +1,97 @@
+// The GPLv3 License (GPLv3)
+//
+// Copyright © 2023 Tushar Maharana
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/* C-calc
+ A calculator written in C.*/
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+union token {
+  char operator;
+  int number;
+};
+
+struct token_stream {
+  char *source;
+  union token *tokens;
+};
+
+bool is_in_set(char character, const char *set_string) {
+  for (int i = 0; i < strlen(set_string); ++i) {
+    if (character == set_string[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+struct token_stream tokenizer(char *expression) {
+  struct token_stream token_stream;
+  const char *operators = "+-";
+  const char *digits = "123456790";
+
+  // making a copy of the expression.
+  token_stream.source = malloc(strlen(expression));
+  strcpy(token_stream.source, expression);
+  // allocate more memory to tokens.
+  token_stream.tokens = malloc(strlen(expression) * 2);
+
+  // allocating memory to a number
+  char *number_string = malloc(strlen(expression) * 2);
+
+  // loop through the expression, and put all the tokens seperately in
+  // the tokens array in our struct.
+  bool is_digits = false;
+  bool is_operator = false;
+  char current_character;
+  int source_index = 0, tokens_index = 0, number_index = 0;
+  int string_length = (int) strlen(token_stream.source);
+  while (source_index <= string_length) {
+    current_character = token_stream.source[source_index];
+
+    is_digits = is_in_set(current_character, digits);
+    is_operator = is_in_set(current_character, operators);
+    if (is_operator || (source_index == string_length)) {
+      if (number_index != 0) {
+	number_string[number_index + 1] = 0;
+	token_stream.tokens[tokens_index] = (union token)atoi(number_string);
+	is_digits = 0;
+	number_index = 0;
+	free(number_string);
+	number_string = malloc(strlen(expression) * 2);
+      } else {
+        token_stream.tokens[tokens_index] = (union token)current_character;
+        ++source_index;
+      }
+      ++tokens_index;
+    }
+    if (is_digits) {
+      number_string[number_index] = current_character;
+      ++number_index;
+      ++source_index;
+    }
+  }
+  return token_stream;
+}
+
+int main(void) {
+  return 0;
+}
